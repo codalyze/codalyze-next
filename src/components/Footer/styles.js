b0412cda346fd869f9ec9bae88d8d266cae75262
Musaib <no_name>
2018-12-20 19:27:34 +0530

    Created tech stack section and designed it. Created footer component and mounted in footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+import * as global from '../../utils/globalStyles';
+
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import HomeBanner from "../../components/HomeBanner";
 import { Title } from "../../utils/globalStyles";
 import Strategy from "../home/strategy";
@@ -15,6 +16,7 @@ const HomePage = () => (
     <SpecializeIn />
     <Clients />
     <TechStack />
+    <Footer />
     {/* <Title>HomePage 1</Title> */}
   </div>
 );
