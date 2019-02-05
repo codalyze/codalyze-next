15afe560018c36021ba7a7f36ede2b8863fcb1e0
Ashishcd <asurve18@yahoo.com>
2019-02-05 19:14:03 +0530

    added find opportunity page

--- src/containers/find-opportunities/styles.js
+++ src/containers/find-opportunities/styles.js
@@ -13,17 +13,120 @@ export const container =  styled.div`
 ${global.container}
 `;
 
+export const openingsRow = styled.div`
+  ${global.fullRow}
+  position: relative;
+   
+`;
+
 export const HomeBannerWrap =  styled.div`
 margin-top: 5%;
 `;
 
-export const bannerSvg = styled.div`
-position: absolute;
+
+export const FlexContainer = styled.div`
+  ${global.contentContainer}
+ 
+`;
+
+export const openingSection = styled.div`
+  position: absolute;
+  top: 24%;
+  width:100%
+`;
+
+export const title = styled.h4`
+  ${global.fontRobotoLight};
+  text-align: center;
+  font-size: 48px;
+  opacity: inherit;
+  margin: 0 0 26px;
+`;
+
+export const openingsList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const openingsBlock = styled.button`
+  
+  margin-bottom: 30px;
+  background: #ffffff;
+  box-shadow: 0px 3px 6px #e3e3e3;
+  border-radius: 10px;
+  width: 30%;
+  margin-right: 10px;
+  padding: 58px 20px;
+  min-height: 95px;
+  text-align:left;
+  border:0;
+`;
+
+export const designationTitle = styled.h5`
+${global.fontRobotoMedium};
+  font-size: 34px;
+  margin: 0 0 10px;
+`;
+
+export const experienceSubTitle = styled.h6`
+${global.fontRobotoLight};
+  font-size: 20px;
+  margin: 0 0 0;
+  width: 65%;
+`;
+
+export const CareerBg = styled.div`
   left: 0;
-  width: 100%; 
+  bottom:0;
+  width: 100%;  
   z-index: -1;
   svg {
     width: 100%;
     height: auto;
-  }  
+  }
+  .a {
+    opacity: 0.5;
+  }
+`;
+
+export const viewMore = styled.button`
+  ${global.mainButton}
+  display: block;
+  margin: 0 auto;
+  margin-top: 106px;
+`;
+
+
+export const paginationWrap = styled.div`
+display:flex;
+flex-direction:row;
+`;
+
+export const paginationFirst = styled.div`
+padding: 10px 15px;
+min-width:15px;
+background: #1C9868;
+color: #ffffff;
+border-right: 1px solid #fff;
+text-align:center;
+`;
+
+export const paginationUl = styled.ul`
+    display: flex;
+    justify-content: space-between;
+`;
+export const paginationLi = styled.li`
+    padding: 10px 15px;
+    list-style: none;
+    background: #000;
+    border-right: 1px solid #fff;
+    text-align: center;
+    color: #fff;
+`;
+
+export const rightImage = styled.div`
+position: absolute;
+right: 0;
+top: 13%;
 `;
