b52586e784c032c4160627956de45f1e66ab9d2a
Ashishcd <asurve18@yahoo.com>
2019-02-05 11:32:48 +0530

    find opportunities

--- src/containers/find-opportunities/findOpportunity.js
+++ src/containers/find-opportunities/findOpportunity.js
@@ -1,12 +1,22 @@
 import React, {Component} from "react";
 import HomeBanner from "../../components/HomeBanner";
 import * as Styled from "./styles";
+import OpportunityBg from "./images/OpportunityBg";
 
 
-export default class findOpportunities extends Component {
+export default class FindOpportunities extends Component {
     render(){
         return(
-            <div></div>
+            <Styled.pageWrapper>
+                <Styled.HomeBannerWrap>
+                 <HomeBanner
+                    title="Find opportunities"         
+        />
+                </Styled.HomeBannerWrap>
+                <Styled.bannerSvg>
+                    <OpportunityBg width="100%" height="100%" viewBox="0 0 2770.359 1953.74" preserveAspectRatio="none"/>
+                </Styled.bannerSvg>
+             </Styled.pageWrapper>
         )
     }
 }
--- src/containers/find-opportunities/styles.js
+++ src/containers/find-opportunities/styles.js
@@ -1,2 +1,29 @@
 import styled from "styled-components";
 import * as global from "../../utils/globalStyles";
+
+export const pageWrapper = styled.div`
+  position: relative;
+`;
+
+export const heading =  styled.div`
+ margin-top:30
+`;
+
+export const container =  styled.div`
+${global.container}
+`;
+
+export const HomeBannerWrap =  styled.div`
+margin-top: 5%;
+`;
+
+export const bannerSvg = styled.div`
+position: absolute;
+  left: 0;
+  width: 100%; 
+  z-index: -1;
+  svg {
+    width: 100%;
+    height: auto;
+  }  
+`;
