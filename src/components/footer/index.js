dc94e59aae0e0c8d5953e46e4891fe906655272b
Musaib <no_name>
2019-02-06 16:55:24 +0530

    Created and designed footer. Linked with pages

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -20,19 +20,73 @@ const footerLinks = [
     href: "career"
   },
   {
+    name: "Contact",
+    href: "#"
+  },
+  {
     name: "Start a project",
     href: "start-a-project"
   }
 ];
 
-const Footer = () => {
-  return (
-    <Styled.containerFluid>
-      <Styled.container>
-          
-      </Styled.container>
-    </Styled.containerFluid>
-  );
-};
+class FooterLink extends React.Component {
+  render() {
+    const { href, name, linkRef, active } = this.props;
+    return (
+      <Link href={href} passHref prefetch>
+        <Styled.ul>
+          <Styled.li ref={linkRef}>{name}</Styled.li>
+        </Styled.ul>
+      </Link>
+    );
+  }
+}
 
-export default Footer;
+export default class Footer extends React.PureComponent {
+  render() {
+    const { top, width, left, right, href } = this.props;
+    return (
+      <Styled.containerFluid>
+        <Styled.container>
+          <Styled.footerRow>
+            <Styled.footerPages>
+              {footerLinks.map((x, index) => {
+                return (
+                  <FooterLink
+                    href={`/${x.href}`}
+                    name={x.name}
+                    key={index}
+                  />
+                );
+              })}
+            </Styled.footerPages>
+            <Styled.footerContent>
+              <Styled.h3>Reach Us At :</Styled.h3>
+              <Styled.h4>
+                Work Square, 1st Floor, Urmi
+                <br /> Corporate Park, Near Solaris - D,
+                <br /> Opp. L&T Gate - 6, Saki Vihar Road,
+                <br /> Chandivali, Mumbai,
+                <br /> Maharashtra 400072
+              </Styled.h4>
+              <Styled.h3>Connect with us</Styled.h3>
+              <Styled.socialImages>
+                <Styled.img src="/static/images/footer/fb.png" />
+                <Styled.img src="/static/images/footer/instagram.png" />
+                <Styled.img src="/static/images/footer/linkedin.png" />
+                <Styled.img src="/static/images/footer/twitter.png" />
+              </Styled.socialImages>
+              <Styled.contactUs href="mailto: mail@codalyze.com">
+                mail@codalyze.com
+              </Styled.contactUs>
+              <br />
+              <Styled.contactUs href="telto: +91-9833-692503">
+                +91- 9833-692503
+              </Styled.contactUs>
+            </Styled.footerContent>
+          </Styled.footerRow>
+        </Styled.container>
+      </Styled.containerFluid>
+    );
+  }
+}
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
-import * as global from '../../utils/globalStyles';
+import styled from "styled-components";
+import * as global from "../../utils/globalStyles";
 
 export const containerFluid = styled.div`
   ${global.fullRow};
+  background: #242424;
 `;
 
 export const container = styled.div`
@@ -10,13 +11,65 @@ export const container = styled.div`
 `;
 
 export const footerRow = styled.div`
-  
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  padding: 65px 0 40px;
 `;
 
 export const footerPages = styled.div`
-  
+  color: #ffffff;
 `;
 
 export const footerContent = styled.div`
-  
+  color: #ffffff;
+  text-align: right;
+`;
+
+export const ul = styled.ul`
+  list-style-type: none;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`;
+
+export const li = styled.li`
+  ${global.fontRobotoLight}
+  font-size: 20px;
+  margin: 0 0 25px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+export const h3 = styled.h3`
+  ${global.fontRobotoMedium}
+  margin: 0 0 10px;
+  font-size: 24px;
+  color: #ffffff;
+`;
+
+export const h4 = styled.h4`
+  ${global.fontRobotoLight}
+  margin: 0 0 30px;
+  font-size: 20px;
+  color: #ffffff;
+`;
+
+export const h5 = styled.h5`
+  ${global.fontRobotoLight}
+  font-size: 20px;
+  color: #ffffff;
+`;
+
+export const contactUs = styled.a`
+  text-decoration: none;
+  ${global.fontRobotoLight}
+  font-size: 20px;
+  color: #ffffff;
+`;
+
+export const socialImages = styled.div``;
+
+export const img = styled.img`
+  margin: 0 0 10px 15px;
 `;
