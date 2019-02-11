23974d76c434763f1ab0941d460e65201ff4b77b
Musaib <no_name>
2019-02-11 16:59:38 +0530

    web developer resume page upload

--- src/containers/web-developer/web-developer.js
+++ src/containers/web-developer/web-developer.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 import * as Styled from "./styles";
 import Description from "./description";
 import JobApplication from "./form";
+import Footer from "../../components/footer";
 
 export default class WebDeveloper extends React.Component {
-    render() {
-        return(
-           <Styled.containerFluid>
-               <Styled.container>
-                   <Styled.h2>Web developer</Styled.h2>
-                   <Description />
-                   <JobApplication />
-               </Styled.container>
-           </Styled.containerFluid>
-        )
-    }
-};
+  render() {
+    return (
+      <Styled.containerFluid>
+        <Styled.container>
+          <Styled.h2>Web developer</Styled.h2>
+          <Description />
+          <JobApplication />
+        </Styled.container>
+        <Footer />
+      </Styled.containerFluid>
+    );
+  }
+}
