879bd61fc5c6db8584498efc50fb0f1a4e3bb87d
Musaib <no_name>
2019-02-20 18:58:04 +0530

    Removed commented code from about us page

--- src/containers/about-us/otherSections.js
+++ src/containers/about-us/otherSections.js
@@ -1,185 +1,179 @@
 import React from "react";
-import * as Styled from './styles';
+import * as Styled from "./styles";
 import TeamMemberBg from "./images/teamMemberBg";
 import LoveStatUpsBg from "./images/loveStartups";
 import HitechBg from "./images/HiTechBg";
 
-
 export const HiTech = () => {
-    return(
-        <Styled.HitechMain>            
-                <HitechBg width="100%" height="100%" viewBox="0 0 1920 2143.35"/>   
-                <Styled.HitechInner>            
-            <Styled.FlexContainer>                                  
-                   <Styled.devisonDiv>
-                            <Styled.leftDiv>
-                            <Styled.imageRight src="/static/images/about-us/Artboard-7.png" />
-                            </Styled.leftDiv>
-                            <Styled.rightDiv>
-                            <Styled.h2>High Tech, Low Risk</Styled.h2>                               
-                               <Styled.p>   
-                               Building custom software is risky and hard, but there’s a way to stack those odds in your favour. It’s all about the right people and process. We create hands-on partnerships with our clients and share a proven process we’ve honed over successful software projects.
-                                   </Styled.p>
-                            </Styled.rightDiv>
-                        </Styled.devisonDiv> 
-                        </Styled.FlexContainer>
-               </Styled.HitechInner> 
-                        {/* <Styled.ProducTeamWrapper> 
-                                <Styled.ProducTeamInner>
-                                    <Styled.FlexContainer>  
-                                        <Styled.devisonDiv>
-                                            <Styled.leftDiv>
-                                            <Styled.h2>Focused Product Teams</Styled.h2> 
-                                            <Styled.p>   
-                                            You’ll be working with a small, dedicated team built to fit the needs of your project. Enjoy deep focus and productive relationships. Partner with us and we'll make your code and team better.
-                                                </Styled.p>                                                
-                                            </Styled.leftDiv>
-                                            <Styled.rightDiv right>
-                                            <Styled.imageRight src="/static/images/about-us/Artboard-5.png" />
-                                            </Styled.rightDiv>
-                                        </Styled.devisonDiv> 
-                                    </Styled.FlexContainer>
-                                </Styled.ProducTeamInner>
-                            </Styled.ProducTeamWrapper>    */}
-        </Styled.HitechMain>
-    )
-}
+  return (
+    <Styled.HitechMain>
+      <HitechBg width="100%" height="100%" viewBox="0 0 1920 2143.35" />
+      <Styled.HitechInner>
+        <Styled.FlexContainer>
+          <Styled.devisonDiv>
+            <Styled.leftDiv>
+              <Styled.imageRight src="/static/images/about-us/Artboard-7.png" />
+            </Styled.leftDiv>
+            <Styled.rightDiv>
+              <Styled.h2>High Tech, Low Risk</Styled.h2>
+              <Styled.p>
+                Building custom software is risky and hard, but there’s a way to
+                stack those odds in your favour. It’s all about the right people
+                and process. We create hands-on partnerships with our clients
+                and share a proven process we’ve honed over successful software
+                projects.
+              </Styled.p>
+            </Styled.rightDiv>
+          </Styled.devisonDiv>
+        </Styled.FlexContainer>
+      </Styled.HitechInner>
+    </Styled.HitechMain>
+  );
+};
 
 export const ProductTeams = () => {
-    return(
-        <Styled.ProducTeamWrapper> 
-            <Styled.ProducTeamInner>
-                                    <Styled.FlexContainer>  
-                                        <Styled.devisonDiv>
-                                            <Styled.leftDiv>
-                                            <Styled.h2>Focused Product Teams</Styled.h2> 
-                                            <Styled.p>   
-                                            You’ll be working with a small, dedicated team built to fit the needs of your project. Enjoy deep focus and productive relationships. Partner with us and we'll make your code and team better.
-                                                </Styled.p>                                                
-                                            </Styled.leftDiv>
-                                            <Styled.rightDiv right>
-                                            <Styled.imageRight src="/static/images/about-us/Artboard-5.png" />
-                                            </Styled.rightDiv>
-                                        </Styled.devisonDiv> 
-                                    </Styled.FlexContainer>
-                                </Styled.ProducTeamInner>
-        </Styled.ProducTeamWrapper>
-    )
-}
+  return (
+    <Styled.ProducTeamWrapper>
+      <Styled.ProducTeamInner>
+        <Styled.FlexContainer>
+          <Styled.devisonDiv>
+            <Styled.leftDiv>
+              <Styled.h2>Focused Product Teams</Styled.h2>
+              <Styled.p>
+                You’ll be working with a small, dedicated team built to fit the
+                needs of your project. Enjoy deep focus and productive
+                relationships. Partner with us and we'll make your code and team
+                better.
+              </Styled.p>
+            </Styled.leftDiv>
+            <Styled.rightDiv right>
+              <Styled.imageRight src="/static/images/about-us/Artboard-5.png" />
+            </Styled.rightDiv>
+          </Styled.devisonDiv>
+        </Styled.FlexContainer>
+      </Styled.ProducTeamInner>
+    </Styled.ProducTeamWrapper>
+  );
+};
 
-const TeamMembers = [{
-    id:1,
+const TeamMembers = [
+  {
+    id: 1,
     image: "/static/images/about-us/Team/1.png",
     name: "Naisheel verdhan",
     position: "Ceo & Founder"
-},
-{
-    id:2,
+  },
+  {
+    id: 2,
     image: "/static/images/about-us/Team/2.png",
     name: "Naisheel verdhan",
     position: "Ceo & Founder"
-},
-{
-    id:3,
+  },
+  {
+    id: 3,
     image: "/static/images/about-us/Team/3.png",
     name: "Naisheel verdhan",
     position: "Ceo & Founder"
-},
-{
-    id:4,
+  },
+  {
+    id: 4,
     image: "/static/images/about-us/Team/4.png",
     name: "Naisheel verdhan",
     position: "Ceo & Founder"
-},
-{
-    id:5,
+  },
+  {
+    id: 5,
     image: "/static/images/about-us/Team/5.png",
     name: "Naisheel verdhan",
     position: "Ceo & Founder"
-},
-{
-    id:6,
+  },
+  {
+    id: 6,
     image: "/static/images/about-us/Team/6.png",
     name: "Naisheel verdhan",
     position: "Ceo & Founder"
-},
-]
+  }
+];
 
 export const TeamMember = () => {
-    return(
-        <Styled.TeamMain>
-            <Styled.TeamBg>
-            <TeamMemberBg width="100%" height="100%" viewBox="0 0 1920.998 700"/>
-            </Styled.TeamBg>
-            <Styled.FlexContainer>
-                    <Styled.h2>
-                    The team that makes <br/>it all possible
-                    </Styled.h2>                
-                     <Styled.TeamPhotowrapper>
-                     { TeamMembers.map(member => (
-                     <Styled.Photo key={member.id}>
-                         <Styled.WrapperBlock>
-                         <Styled.TeamMemberImage src={member.image}/>
-                         <Styled.TeamMemberName>
-                             {member.name}
-                         </Styled.TeamMemberName>
-                         <Styled.TeamPosition>
-                             {member.position}
-                         </Styled.TeamPosition>
-                         </Styled.WrapperBlock>
-                     </Styled.Photo>
-                    ))  }
-                     </Styled.TeamPhotowrapper>
-                <Styled.ButtonWrapper><Styled.Button>START A PROJECT</Styled.Button></Styled.ButtonWrapper>
-                </Styled.FlexContainer>
-            </Styled.TeamMain>
-    )
-}
+  return (
+    <Styled.TeamMain>
+      <Styled.TeamBg>
+        <TeamMemberBg width="100%" height="100%" viewBox="0 0 1920.998 700" />
+      </Styled.TeamBg>
+      <Styled.FlexContainer>
+        <Styled.h2>
+          The team that makes <br />
+          it all possible
+        </Styled.h2>
+        <Styled.TeamPhotowrapper>
+          {TeamMembers.map(member => (
+            <Styled.Photo key={member.id}>
+              <Styled.WrapperBlock>
+                <Styled.TeamMemberImage src={member.image} />
+                <Styled.TeamMemberName>{member.name}</Styled.TeamMemberName>
+                <Styled.TeamPosition>{member.position}</Styled.TeamPosition>
+              </Styled.WrapperBlock>
+            </Styled.Photo>
+          ))}
+        </Styled.TeamPhotowrapper>
+        <Styled.ButtonWrapper>
+          <Styled.Button>START A PROJECT</Styled.Button>
+        </Styled.ButtonWrapper>
+      </Styled.FlexContainer>
+    </Styled.TeamMain>
+  );
+};
 
 export const LoveStatUps = () => {
-return(
-    <Styled.LoveStartupsDiv> 
-                <LoveStatUpsBg width="100%" height="100%" viewBox="0 0 1920.997 1275.951"/>
-                <Styled.innerDiv>
-        <Styled.FlexContainer>                     
-            <Styled.devisonDiv>
-                     <Styled.leftDiv>
-                     <Styled.imageRight src="/static/images/about-us/Artboard-6.png" />
-                     </Styled.leftDiv>
-                     <Styled.rightDiv>
-                     <Styled.h2>We Love Start-Ups</Styled.h2>
-                        
-                        <Styled.listUl>
-                            <Styled.li>
-                                <Styled.circleDiv></Styled.circleDiv>
-                                We share your passion for changing the world</Styled.li>
-                            <Styled.li>
-                            <Styled.circleDiv></Styled.circleDiv>
-                                We've helped ideas grow in the past and it feels beautiful</Styled.li>
-                            <Styled.li>
-                            <Styled.circleDiv></Styled.circleDiv>
-                                If you run a start-up and are looking for a tech partner, please say Hi!</Styled.li>
-                        </Styled.listUl>                                                                        
-                     </Styled.rightDiv>
-                 </Styled.devisonDiv>             
-        </Styled.FlexContainer>
-        </Styled.innerDiv>
-    </Styled.LoveStartupsDiv>
-)
-}
+  return (
+    <Styled.LoveStartupsDiv>
+      <LoveStatUpsBg
+        width="100%"
+        height="100%"
+        viewBox="0 0 1920.997 1275.951"
+      />
+      <Styled.innerDiv>
+        <Styled.FlexContainer>
+          <Styled.devisonDiv>
+            <Styled.leftDiv>
+              <Styled.imageRight src="/static/images/about-us/Artboard-6.png" />
+            </Styled.leftDiv>
+            <Styled.rightDiv>
+              <Styled.h2>We Love Start-Ups</Styled.h2>
 
+              <Styled.listUl>
+                <Styled.li>
+                  <Styled.circleDiv />
+                  We share your passion for changing the world
+                </Styled.li>
+                <Styled.li>
+                  <Styled.circleDiv />
+                  We've helped ideas grow in the past and it feels beautiful
+                </Styled.li>
+                <Styled.li>
+                  <Styled.circleDiv />
+                  If you run a start-up and are looking for a tech partner,
+                  please say Hi!
+                </Styled.li>
+              </Styled.listUl>
+            </Styled.rightDiv>
+          </Styled.devisonDiv>
+        </Styled.FlexContainer>
+      </Styled.innerDiv>
+    </Styled.LoveStartupsDiv>
+  );
+};
 
 const AboutOtherSections = () => {
-    return(
-        <Styled.mainDiv>            
-            <HiTech/>
-            <ProductTeams/>
-            <LoveStatUps/>            
-            <TeamMember/>
-        </Styled.mainDiv>
-        
-    )      
-    
-}
+  return (
+    <Styled.mainDiv>
+      <HiTech />
+      <ProductTeams />
+      <LoveStatUps />
+      <TeamMember />
+    </Styled.mainDiv>
+  );
+};
 
 export default AboutOtherSections;
