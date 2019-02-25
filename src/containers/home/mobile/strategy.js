abfdd19620f2cc3217e4f1c12329eebc411454e4
Musaib <no_name>
2019-02-25 19:38:47 +0530

    Mobile homepage banner and strategy section development

---
+++ src/containers/home/mobile/strategy.js
@@ -0,0 +1,71 @@
+import React from "react";
+import * as Styled from "./styles";
+
+import Link from "next/link";
+
+const StrategyStats = [
+  {
+    number: "14",
+    title: "Team Players"
+  },
+  {
+    number: "5+",
+    title: "Awards"
+  },
+  {
+    number: "76",
+    title: "Applications"
+  },
+  {
+    number: "20+",
+    title: "Clients"
+  },
+  {
+    number: "30+",
+    title: "Projects"
+  },
+  {
+    number: "2500+",
+    title: "Hours of coding"
+  }
+];
+
+const ServiceBtn = ({ href }) => (
+  <Link href={href} passHref prefetch>
+    <Styled.ourWorkBtn>our work</Styled.ourWorkBtn>
+  </Link>
+);
+
+const Strategy = () => (
+  <Styled.strategyRow>
+    <Styled.strategyBlockTwo>
+        <Styled.strategyTitle>
+          Strategy led solutions for fast moving companies.
+        </Styled.strategyTitle>
+        <Styled.strategySubTitle>
+          The proof is the pudding. While there isn't a way to eat our
+          interfaces (yet), you can sample a large offering of non-perishable
+          case studies.
+        </Styled.strategySubTitle>
+        
+      </Styled.strategyBlockTwo>
+
+      <Styled.strategyBlockOne>
+        {StrategyStats.map((x, id) => {
+          return (
+            <Styled.strategyStatsBlock key={id}>
+              <Styled.strategyStats>
+                <Styled.strategyStatsNum>{x.number}</Styled.strategyStatsNum>
+                <Styled.strategyStatsTitle>{x.title}</Styled.strategyStatsTitle>
+              </Styled.strategyStats>
+            </Styled.strategyStatsBlock>
+          );
+        })}
+      </Styled.strategyBlockOne>
+
+      <ServiceBtn href={"/work"}/>
+   
+  </Styled.strategyRow>
+)
+
+export default Strategy;
--- src/containers/home/mobile/styles.js
+++ src/containers/home/mobile/styles.js
@@ -3,5 +3,92 @@ import * as global from "../../../utils/globalStyles";
 
 export const commonDiv = styled.div``;
 
+export const HomeMobileBgSvg = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  top: 37%;
+  z-index: -1;
+  svg {
+    width: 100%;
+    height: auto;
+  }
+  .a {
+    opacity: 0.4;
+  }
+`;
+
+export const strategyRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 35px;
+`;
+
+
+export const strategyBlockOne = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 50px;
+  align-items: center;
+  justify-items: center;
+  margin-left: -10px;
+
+`;
+
+export const strategyBlockTwo = styled.div``;
+
+export const strategyInfoNum = styled.h3`
+  color: #fff;
+  font-size: 25px;
+`;
+
+export const strategyTitle = styled.h2`
+  ${global.mobileTitle}
+  color: #000;
+  margin: 0 0 15px;
+`;
+
+export const strategySubTitle = styled.h4`
+  ${global.fontRobotoLight};
+  color: #1e1e1e;
+  font-size: 24px;
+  margin: 0 0 50px;
+  line-height: 35px;
+  letter-spacing: 1px;
+  font-weight: inherit;
+`;
+
+
+export const strategyStatsBlock = styled.div`
+  margin-bottom: 25%;
+`;
+
+export const strategyStats = styled.div`
+  text-align: center;
+`;
+
+export const strategyStatsNum = styled.h5`
+  ${global.fontRobotoLight};
+  color: #1e1e1e;
+  font-size: 50px;
+  margin-bottom: 10px;
+  margin: 0;
+  font-weight: inherit;
+`;
+
+export const strategyStatsTitle = styled.h6`
+  ${global.fontRobotoLight};
+  color: #111111;
+  font-size: 25px;
+  margin: 0;
+`;
+
+export const ourWorkBtn = styled.button`
+  ${global.mainButton}
+  margin: 0 auto;
+`;
+
+
 
 
