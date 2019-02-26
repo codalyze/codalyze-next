ce3e60e9f043f8401e6f2acdbf3e7f09087984c7
Musaib <no_name>
2019-02-26 15:31:43 +0530

    Added viewport. Fixed banner and font issues. Redsigned banner and strategy section

---
+++ src/containers/home/mobile/specialize.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as Styled from "./styles";
+import Link from "next/link";
+
+const SpecializeInMobile = () => (
+
+    <Styled.pageWrapper>
+        dsfdfsf
+    </Styled.pageWrapper>
+)
+
+export default SpecializeInMobile;
