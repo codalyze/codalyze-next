912bb023d50c162ce25dc13bf560a92287aae4cd
Musaib <no_name>
2019-03-08 19:04:25 +0530

    Created our team section and redesigned the page

--- src/containers/about-us/mobile/styles.js
+++ src/containers/about-us/mobile/styles.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import * as global from "../../../utils/globalStyles";
 
+export const pageWrapper = styled.div`
+  position: relative;
+`;
+
+
 export const title = styled.h1`
   ${global.mobileTitle}
   padding: 0 35px 0;
@@ -67,42 +72,123 @@ export const aboutBannerMobile = styled.div`
   }
 `;
 
-export const techStackContainer = styled.div`
+export const teamContainer = styled.div`
   position: relative;
   padding: 0 35px 50px;
 `;
 
-// export const techMobileBgBlock = styled.div`
-//   position: absolute;
-//   left: 0;
-//   width: 100%;
-//   z-index: -1;
-//   bottom: 0;
-//   svg {
-//     width: 100%;
-//     height: auto;
-//   }
-//   .a {
-//   }
-// `;
+export const teamStarBgContainer = styled.div`
+  position: absolute;
+  left: -50px;
+  top: -90px;
+  width: 158px;
+  z-index: -2;
+  svg {
+    width: 100%;
+    height: auto;
+  }
+  .a {
+  }
+`;
 
-export const techStackRow = styled.div`
+export const teamRow = styled.div`
   display: flex;
   flex-wrap: wrap;
-  text-align: center;
-  align-items: center;
+  justify-content: space-between;
   padding-top: 40px;
 `;
 
-export const techStackBlock = styled.div`
-  ${global.twoBlocks}
+export const teamBlock = styled.div`
   padding-bottom: 50px;
 `;
 
-export const techStackLogo = styled.img``;
+export const teamMemberImg = styled.img``;
+
+export const teamTitle = styled.h5`
+  ${global.fontRobotoMedium}
+  font-size: 15px;
+  margin: 0 0 0;
+`;
+
+export const teamPosition = styled.h5`
+  ${global.fontRobotoLight}
+  font-style: italic;
+  font-size: 15px;
+  margin: 0 0 0;
+`;
 
 export const greenBtn = styled.button`
   ${global.mobileButton}
   margin: 0 auto;
   display: block;
+`;
+
+export const teamBgContainer = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0px;
+  width: 100%;
+  z-index: -2;
+  svg {
+    width: 100%;
+    height: auto;
+  }
+  .a {
+  }
+`;
+
+export const optBgContainer = styled.div`
+  position: absolute;
+  right: -70px;
+  top: -70px;
+  width: 158px;
+  z-index: -2;
+  svg {
+    width: 100%;
+    height: auto;
+  }
+  .a {
+  }
+`;
+
+export const highBgContainer = styled.div`
+  position: absolute;
+  right: 0;
+  top: -300px;
+  width: 100%;
+  z-index: -2;
+  svg {
+    width: 100%;
+    height: auto;
+  }
+  .a {
+  }
+`;
+
+export const focusedBgContainer = styled.div`
+  position: absolute;
+  right: -17px;
+  top: 90px;
+  width: 158px;
+  z-index: -2;
+  svg {
+    width: 100%;
+    height: auto;
+  }
+  .a {
+  }
+`;
+
+export const loveBgContainer = styled.div`
+  position: absolute;
+  right: 0;
+  top: -30px;
+  width: 100%;
+  z-index: -2;
+  svg {
+    width: 100%;
+    height: auto;
+  }
+  .a {
+  }
 `;
--- src/containers/about-us/mobile/team.js
+++ src/containers/about-us/mobile/team.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
 import * as Styled from "./styles";
 import Link from "next/link";
+import Star from './images/star';
+import TeamBg from './images/teambottomBg';
 
 const OurServices = ({ href }) => (
     <Link href={href} passHref prefetch>
@@ -8,35 +10,67 @@ const OurServices = ({ href }) => (
     </Link>
   );
   
-  const techLogoMob = [
-
-    "../static/images/mobile/about/gaurav.png",
-    "../static/images/mobile/about/harry.png",
-    "../static/images/mobile/about/surbhi.png",
-    "../static/images/mobile/about/naisheel.png",
-    "../static/images/mobile/about/rakesh.png",
-    "../static/images/mobile/about/rishabh.png"
+  const teamMembers = [
+    {
+      image:"../static/images/mobile/about/naisheel.png",
+      name:"Naisheel",
+      designation: "CEO & Founder"
+    },
+    {
+      image:"../static/images/mobile/about/harry.png",
+      name:"Harry",
+      designation: "CEO & Founder"
+    },
+    {
+      image:"../static/images/mobile/about/gaurav.png",
+      name:"Gaurav",
+      designation: "CEO & Founder"
+    },
+    {
+      image:"../static/images/mobile/about/rakesh.png",
+      name:"Rakesh",
+      designation: "CEO & Founder"
+    },
+    {
+      image:"../static/images/mobile/about/rishabh.png",
+      name:"Rishabh",
+      designation: "CEO & Founder"
+    },
+    {
+      image:"../static/images/mobile/about/surbhi.png",
+      name:"Surbhi",
+      designation: "CEO & Founder"
+    }
   ];
 
 const teamMobile = () => (
 <Fragment>
-    <Styled.techStackContainer>
-      {/* <Styled.techMobileBgBlock>
-        <TechStackBg 
+    <Styled.teamContainer>
+      <Styled.teamStarBgContainer>
+        <Star 
           width="100%"
           height="100%"
-          viewBox="400 0 1050.371 1400.229"
+          viewBox="0 0 158.595 154.538"
         />
-      </Styled.techMobileBgBlock> */}
-      <Styled.techStackRow>
-        {techLogoMob.map((logo, index) => (
-          <Styled.techStackBlock key={index}>
-            <Styled.techStackLogo src={logo} />
-          </Styled.techStackBlock>
+      </Styled.teamStarBgContainer>
+      <Styled.teamRow>
+        {teamMembers.map((member, index) => (
+          <Styled.teamBlock key={index}>
+            <Styled.teamMemberImg src={member.image} />
+            <Styled.teamTitle>{member.name}</Styled.teamTitle>
+            <Styled.teamPosition>{member.designation}</Styled.teamPosition>
+          </Styled.teamBlock>
         ))}
-      </Styled.techStackRow>
+      </Styled.teamRow>
       <OurServices href={"/start-a-project"} />
-    </Styled.techStackContainer>
+      <Styled.teamBgContainer>
+        <TeamBg 
+          width="100%"
+          height="100%"
+          viewBox="200 0 500.763 450.412"
+        />
+      </Styled.teamBgContainer>
+    </Styled.teamContainer>
   </Fragment>
 );
 
