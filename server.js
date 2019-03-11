34f0b9fb83c93a27df096f0365089388e7b35507
nverdhan <nverdhan@gmail.com>
2019-03-11 15:42:54 +0530

    enable compression

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const { createServer } = require('http');
+const express = require('express');
 const path = require('path');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dir: './src', dev });
 const handle = app.getRequestHandler();
 const PORT = process.env.PORT || 3000;
+const compression = require('compression');
 
 app.prepare().then(_ => {
-  const server = createServer((req, res) => {
+  const app = express();
+  app.use(compression());
+  app.use((req, res) => {
     if (req.url === '/sw.js') {
       app.serveStatic(req, res, path.resolve('./src/static/sw.js'));
     } else {
       handle(req, res);
     }
-  })
-  server.listen(PORT, err => {
+  });
+  createServer(app).listen(PORT, err => {
     if (err) throw err
     console.log(`> App running on port ${PORT}`);
   })
-})
-.catch(e => {
-  console.log(e);
 });
-
