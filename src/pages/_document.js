9bf4bfb249c630196a26fbed4b83c9bf36dac02c
g1aurav <gauravkumar0154@gmail.com>
2019-03-13 00:08:40 +0530

    Initialized react helmet

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import Helmet from 'react-helmet';
+
 import { ServerStyleSheet } from 'styled-components';
 import transitionStyles from '../styles/transitions';
 
@@ -7,13 +9,20 @@ export default class MyDocument extends Document {
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    return { ...page, styleTags, helmet: Helmet.renderStatic() };
+  }
+
+  get helmetHeadComponents () {
+    return Object.keys(this.props.helmet)
+      .filter(el => el !== 'htmlAttributes' && el !== 'bodyAttributes')
+      .map(el => this.props.helmet[el].toComponent())
   }
 
   render () {
     return (
       <html lang="en">
         <Head>
+          {this.helmetHeadComponents}
           {this.props.styleTags}
           <style dangerouslySetInnerHTML={{ __html: transitionStyles }} />
           <meta name="theme-color" content="#55a7a2" />
