2cd142831f89994134f351ed06e79b58ca13af82
g1aurav <gauravkumar0154@gmail.com>
2019-03-12 16:14:57 +0530

    Added raect-reveal to about pages

--- src/containers/about-us/desktop/otherSections.js
+++ src/containers/about-us/desktop/otherSections.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Fade from 'react-reveal/Fade';
+
 import * as Styled from "./styles";
 import TeamMemberBg from "./images/teamMemberBg";
 
@@ -7,118 +9,126 @@ import {SocialWrapper} from "../commons/styles";
 
 export const HiTech = () => {
   return (
-    <Styled.SectionWrapper>
-        <Styled.FlexContainer>
-          <Styled.devisonDiv>
-            <Styled.leftDiv>
-              <img src="/static/images/about-us/Artboard-7.png" />
-            </Styled.leftDiv>
-            <Styled.rightDiv>
-              <Styled.h2>High Tech, Low Risk</Styled.h2>
-              <Styled.p>
-                Building custom software is risky and hard, but there’s a way to
-                stack those odds in your favour. It’s all about the right people
-                and process. We create hands-on partnerships with our clients
-                and share a proven process we’ve honed over successful software
-                projects.
-              </Styled.p>
-            </Styled.rightDiv>
-          </Styled.devisonDiv>
-        </Styled.FlexContainer>
-    </Styled.SectionWrapper>
+    <Fade>
+      <Styled.SectionWrapper>
+          <Styled.FlexContainer>
+            <Styled.devisonDiv>
+              <Styled.leftDiv>
+                <img src="/static/images/about-us/Artboard-7.png" />
+              </Styled.leftDiv>
+              <Styled.rightDiv>
+                <Styled.h2>High Tech, Low Risk</Styled.h2>
+                <Styled.p>
+                  Building custom software is risky and hard, but there’s a way to
+                  stack those odds in your favour. It’s all about the right people
+                  and process. We create hands-on partnerships with our clients
+                  and share a proven process we’ve honed over successful software
+                  projects.
+                </Styled.p>
+              </Styled.rightDiv>
+            </Styled.devisonDiv>
+          </Styled.FlexContainer>
+      </Styled.SectionWrapper>
+    </Fade>
   );
 };
 
 export const ProductTeams = () => {
   return (
-    <Styled.SectionWrapper>
-      <Styled.ProducTeamInner>
-        <Styled.FlexContainer>
-          <Styled.devisonDiv>
-            <Styled.leftDiv>
-              <Styled.h2>Focused Product Teams</Styled.h2>
-              <Styled.p>
-                You’ll be working with a small, dedicated team built to fit the
-                needs of your project. Enjoy deep focus and productive
-                relationships. Partner with us and we'll make your code and team
-                better.
-              </Styled.p>
-            </Styled.leftDiv>
-            <Styled.rightDiv right>
-              <img src="/static/images/about-us/Artboard-5.png" />
-            </Styled.rightDiv>
-          </Styled.devisonDiv>
-        </Styled.FlexContainer>
-      </Styled.ProducTeamInner>
-    </Styled.SectionWrapper>
+    <Fade>
+      <Styled.SectionWrapper>
+        <Styled.ProducTeamInner>
+          <Styled.FlexContainer>
+            <Styled.devisonDiv>
+              <Styled.leftDiv>
+                <Styled.h2>Focused Product Teams</Styled.h2>
+                <Styled.p>
+                  You’ll be working with a small, dedicated team built to fit the
+                  needs of your project. Enjoy deep focus and productive
+                  relationships. Partner with us and we'll make your code and team
+                  better.
+                </Styled.p>
+              </Styled.leftDiv>
+              <Styled.rightDiv right>
+                <img src="/static/images/about-us/Artboard-5.png" />
+              </Styled.rightDiv>
+            </Styled.devisonDiv>
+          </Styled.FlexContainer>
+        </Styled.ProducTeamInner>
+      </Styled.SectionWrapper>
+    </Fade>
   );
 };
 
 export const TeamMember = () => {
   return (
-    <Styled.TeamWrapper>
-      <Styled.TeamBg>
-        <TeamMemberBg width="100%" height="100%" viewBox="0 0 1920.998 700" />
-      </Styled.TeamBg>
-      <Styled.FlexContainer>
-        <Styled.h2>
-          The team that makes <br />
-          it all possible
-        </Styled.h2>
-        <Styled.TeamPhotowrapper>
-          {teamMembers.map(member => (
-            <Styled.Photo key={member.id}>
-              <Styled.WrapperBlock>
-                <Styled.TeamMemberImage src={member.image} />
-                <Styled.TeamMemberName>{member.name}</Styled.TeamMemberName>
-                <Styled.TeamPosition>{member.position}</Styled.TeamPosition>
-                <SocialWrapper>
-                  {member.socialButtons.map(social =>
-                    <a href={social.url} target="_blank" key={social.type}>
-                      <img src={socialIcons[social.type]} height="24" height="24" />
-                    </a>
-                    )}
-                </SocialWrapper>
-              </Styled.WrapperBlock>
-            </Styled.Photo>
-          ))}
-        </Styled.TeamPhotowrapper>
-        <Styled.ButtonWrapper>
-          <Styled.Button>START A PROJECT</Styled.Button>
-        </Styled.ButtonWrapper>
-      </Styled.FlexContainer>
-    </Styled.TeamWrapper>
+    <Fade>
+      <Styled.TeamWrapper>
+        <Styled.TeamBg>
+          <TeamMemberBg width="100%" height="100%" viewBox="0 0 1920.998 700" />
+        </Styled.TeamBg>
+        <Styled.FlexContainer>
+          <Styled.h2>
+            The team that makes <br />
+            it all possible
+          </Styled.h2>
+          <Styled.TeamPhotowrapper>
+            {teamMembers.map(member => (
+              <Styled.Photo key={member.id}>
+                <Styled.WrapperBlock>
+                  <Styled.TeamMemberImage src={member.image} />
+                  <Styled.TeamMemberName>{member.name}</Styled.TeamMemberName>
+                  <Styled.TeamPosition>{member.position}</Styled.TeamPosition>
+                  <SocialWrapper>
+                    {member.socialButtons.map(social =>
+                      <a href={social.url} target="_blank" key={social.type}>
+                        <img src={socialIcons[social.type]} height="24" height="24" />
+                      </a>
+                      )}
+                  </SocialWrapper>
+                </Styled.WrapperBlock>
+              </Styled.Photo>
+            ))}
+          </Styled.TeamPhotowrapper>
+          <Styled.ButtonWrapper>
+            <Styled.Button>START A PROJECT</Styled.Button>
+          </Styled.ButtonWrapper>
+        </Styled.FlexContainer>
+      </Styled.TeamWrapper>
+    </Fade>
   );
 };
 
 export const LoveStatUps = () => {
   return (
-    <Styled.FlexContainer>
-      <Styled.devisonDiv>
-        <Styled.leftDiv>
-          <img src="/static/images/about-us/Artboard-6.png" />
-        </Styled.leftDiv>
-        <Styled.rightDiv>
-          <Styled.h2>We Love Start-Ups</Styled.h2>
+    <Fade>
+      <Styled.FlexContainer>
+        <Styled.devisonDiv>
+          <Styled.leftDiv>
+            <img src="/static/images/about-us/Artboard-6.png" />
+          </Styled.leftDiv>
+          <Styled.rightDiv>
+            <Styled.h2>We Love Start-Ups</Styled.h2>
 
-          <Styled.listUl>
-            <Styled.li>
-              <Styled.circleDiv />
-              We share your passion for changing the world
-            </Styled.li>
-            <Styled.li>
-              <Styled.circleDiv />
-              We've helped ideas grow in the past and it feels beautiful
-            </Styled.li>
-            <Styled.li>
-              <Styled.circleDiv />
-              If you run a start-up and are looking for a tech partner,
-              please say Hi!
-            </Styled.li>
-          </Styled.listUl>
-        </Styled.rightDiv>
-      </Styled.devisonDiv>
-    </Styled.FlexContainer>
+            <Styled.listUl>
+              <Styled.li>
+                <Styled.circleDiv />
+                We share your passion for changing the world
+              </Styled.li>
+              <Styled.li>
+                <Styled.circleDiv />
+                We've helped ideas grow in the past and it feels beautiful
+              </Styled.li>
+              <Styled.li>
+                <Styled.circleDiv />
+                If you run a start-up and are looking for a tech partner,
+                please say Hi!
+              </Styled.li>
+            </Styled.listUl>
+          </Styled.rightDiv>
+        </Styled.devisonDiv>
+      </Styled.FlexContainer>
+    </Fade>
   );
 };
 
--- src/containers/about-us/mobile/aboutUsMobile.js
+++ src/containers/about-us/mobile/aboutUsMobile.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import Fade from 'react-reveal/Fade';
 import * as Styled from "./styles";
 import Banner from "../../../components/HomeBannerMobile";
 import AboutComponent from "./aboutComponent";
@@ -19,67 +20,78 @@ export default class AboutUsMobile extends Component {
           subTitle="We aid you in achieving a tight deadline and help you get to market fast."
           titleBreak
         />
-        <Styled.pageWrapper>
-          <Styled.focusedBgContainer>
-            <Star width="100%" height="100%" viewBox="0 0 158.595 154.538" />
-          </Styled.focusedBgContainer>
-          <AboutComponent
-            headingOne="Who We Are"
-            content="Based out of Powai, Mumbai, Codalyze is a diverse community of people committed to using modern JavaScript and software development best practices to build beautiful web & mobile applications for our clients. We are dedicated to helping startups and enterprises move to the modern web."
-          />
-        </Styled.pageWrapper>
-        <Styled.pageWrapper>
-          <Styled.optBgContainer>
-            <Star width="100%" height="100%" viewBox="0 0 158.595 154.538" />
-          </Styled.optBgContainer>
-          <AboutComponent
-            headingOne="Optimize Approach"
-            content="We aid you in achieving a tight deadline and help you get to market fast. We design and develop web and mobile apps for growth-minded companies - both startups and big enterprises."
-            image="/static/images/mobile/about/optimizeMobile.png"
-          />
-        </Styled.pageWrapper>
-
-        <Styled.pageWrapper>
-          <Styled.highBgContainer>
-            <HighTechBg
-              width="100%"
-              height="100%"
-              viewBox="100 0 300.267 758.327"
+        <Fade>
+          <Styled.pageWrapper>
+            <Styled.focusedBgContainer>
+              <Star width="100%" height="100%" viewBox="0 0 158.595 154.538" />
+            </Styled.focusedBgContainer>
+            <AboutComponent
+              headingOne="Who We Are"
+              content="Based out of Powai, Mumbai, Codalyze is a diverse community of people committed to using modern JavaScript and software development best practices to build beautiful web & mobile applications for our clients. We are dedicated to helping startups and enterprises move to the modern web."
             />
-          </Styled.highBgContainer>
-          <AboutComponent
-            headingOne="High Tech, Low Risk"
-            content="Building custom software is risky and hard, but there’s a way to stack those odds in your favour. It’s all about the right people and process. We create hands-on partnerships with our clients and share a proven process we’ve honed over successful software projects."
-            image="/static/images/mobile/about/hightechMobile.png"
-          />
-        </Styled.pageWrapper>
-        <Styled.pageWrapper>
-          <Styled.focusedBgContainer>
-            <Star width="100%" height="100%" viewBox="0 0 158.595 154.538" />
-          </Styled.focusedBgContainer>
-          <AboutComponent
-            headingOne="Focused Product Teams"
-            content="You’ll be working with a small, dedicated team built to fit the needs of your project. Enjoy deep focus and productive relationships. Partner with us and we'll make your code and team better."
-            image="/static/images/mobile/about/teamMobile.png"
-          />
-        </Styled.pageWrapper>
-        <Styled.pageWrapper>
-            <Styled.loveBgContainer>
-                <LoveBg
-                 width="100%" height="100%" viewBox="100 0 370.462 515.44"
-                />
-            </Styled.loveBgContainer>
-          <AboutComponent
-            headingOne="We Love Start-Ups"
-            contentTwo="We share your passion for changing the world"
-            contentThree="We've helped ideas grow in the past and it feels beautiful"
-            contentFour="If you run a start-up and are looking for a tech partner, please say Hi!"
-            image="/static/images/mobile/about/startupMobile.png"
-            hide
-            multiple
-          />
-        </Styled.pageWrapper>
-        <TeamMobile />
+          </Styled.pageWrapper>
+        </Fade>
+        <Fade>
+          <Styled.pageWrapper>
+            <Styled.optBgContainer>
+              <Star width="100%" height="100%" viewBox="0 0 158.595 154.538" />
+            </Styled.optBgContainer>
+            <AboutComponent
+              headingOne="Optimize Approach"
+              content="We aid you in achieving a tight deadline and help you get to market fast. We design and develop web and mobile apps for growth-minded companies - both startups and big enterprises."
+              image="/static/images/mobile/about/optimizeMobile.png"
+            />
+          </Styled.pageWrapper>
+        </Fade>
+        <Fade>
+          <Styled.pageWrapper>
+            <Styled.highBgContainer>
+              <HighTechBg
+                width="100%"
+                height="100%"
+                viewBox="100 0 300.267 758.327"
+              />
+            </Styled.highBgContainer>
+            <AboutComponent
+              headingOne="High Tech, Low Risk"
+              content="Building custom software is risky and hard, but there’s a way to stack those odds in your favour. It’s all about the right people and process. We create hands-on partnerships with our clients and share a proven process we’ve honed over successful software projects."
+              image="/static/images/mobile/about/hightechMobile.png"
+            />
+          </Styled.pageWrapper>
+        </Fade>
+        <Fade>
+          <Styled.pageWrapper>
+            <Styled.focusedBgContainer>
+              <Star width="100%" height="100%" viewBox="0 0 158.595 154.538" />
+            </Styled.focusedBgContainer>
+            <AboutComponent
+              headingOne="Focused Product Teams"
+              content="You’ll be working with a small, dedicated team built to fit the needs of your project. Enjoy deep focus and productive relationships. Partner with us and we'll make your code and team better."
+              image="/static/images/mobile/about/teamMobile.png"
+            />
+          </Styled.pageWrapper>
+        </Fade>
+        <Fade>
+          <Styled.pageWrapper>
+              <Styled.loveBgContainer>
+                  <LoveBg
+                  width="100%" height="100%" viewBox="100 0 370.462 515.44"
+                  />
+              </Styled.loveBgContainer>
+            <AboutComponent
+              headingOne="We Love Start-Ups"
+              contentTwo="We share your passion for changing the world"
+              contentThree="We've helped ideas grow in the past and it feels beautiful"
+              contentFour="If you run a start-up and are looking for a tech partner, please say Hi!"
+              image="/static/images/mobile/about/startupMobile.png"
+              hide
+              multiple
+            />
+          </Styled.pageWrapper>
+        </Fade>
+        <Fade>
+          <TeamMobile />
+        </Fade>
         <Footer />
       </Fragment>
     );
