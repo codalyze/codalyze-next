45d29fa0320f0ce44e604c3ce6316427c30ef92e
Aura313 <tanyashah.395@gmail.com>
2019-09-04 21:34:47 +0530

    Blog page

---
+++ src/containers/blogs/images/workBannerBg.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const WorkBannerMobileBg = props => (
+  <svg
+    preserveAspectRatio="none"
+    width="674.125"
+    height="484.329"
+    viewBox="100 0 450 484.329"
+    {...props}
+  >
+    <defs>
+      <linearGradient
+        id="a"
+        x1="0.5"
+        x2="0.5"
+        y2="1"
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset="0" stopColor="#eeeff5" />
+        <stop offset="1" stopColor="#f6eeed" />
+      </linearGradient>
+    </defs>
+    <path
+      d="M7973.555-5700.833s-5.118-32.092-105.649,3.391-92.785,105.961-197.1,122.83-186.267-104.536-249.245-32.375-2.67,321.018-2.67,321.018,153.533,26.684,268.878,1.248,241.574-96.1,321.241-140.186-33.87,20.719-7.924-56.32S7973.555-5700.833,7973.555-5700.833Z"
+      transform="matrix(0.998, 0.07, -0.07, 0.998, -7742.731, 5184.522)"
+      fill="url(#a)"
+    />
+  </svg>
+);
+
+const WorkBannerDesktopBg = props => (
+  <svg
+    preserveAspectRatio="none"
+    width={2298.414}
+    height={1477.886}
+    viewBox="200 0 1950 1477.886"
+    {...props}>
+    <defs>
+      <linearGradient
+        id="prefix__a"
+        x1={0.5}
+        x2={0.5}
+        y2={1}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#eeeff5" />
+        <stop offset={1} stopColor="#f6eeed" />
+      </linearGradient>
+    </defs>
+    <path
+      d="M9489-5672.9s-18.956-99.448-391.264 10.511-343.619 328.363-729.926 380.64-710.866-107.522-870.021-80.5-62.923 974.973-62.923 974.973l975.951-58.557s725.207-252.246 732.714-256.75 351.344-51.05 447.437-289.782S9489-5672.9 9489-5672.9z"
+      transform="matrix(.998 .07 -.07 .998 -7694.035 5183.605)"
+      fill="url(#prefix__a)"
+    />
+  </svg>
+);
+
+const WorkBannerBg = props =>
+  props.mq === "desktop" ? (
+    <WorkBannerDesktopBg {...props} />
+  ) : (
+    <WorkBannerMobileBg {...props} />
+  );
+
+export default WorkBannerBg;
---
+++ src/containers/blogs/styles.js
@@ -0,0 +1,212 @@
+import styled from "styled-components";
+import * as global from "../../utils/globalStyles";
+import { globalAgent } from "https";
+
+export const containerFluid = styled.div`
+  ${global.fullRow};
+`;
+
+export const container = styled.div`
+  ${global.mainContainer};
+`;
+
+export const blogContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 60%;
+  margin-bottom: 20px;
+`;
+
+export const h1 = styled.h1`
+  ${global.fontRobotoThin}
+  font-size: 18vw;
+  text-align: center;
+  margin: 100px 0 80px;
+`;
+
+export const h2 = styled.h2`
+  ${global.fontRobotoThin}
+  font-size: 25vw;
+  color: #d0ddee;
+  margin: 0 0;
+  position: absolute;
+  left: -7%;
+`;
+
+export const h3 = styled.h3`
+  padding: 72px 0 0;
+  ${global.fontFreightDispBold};
+  font-size: 96px;
+`;
+
+export const h4 = styled.h4`
+  ${global.fontFreightDispBold};
+  font-size: 50px;
+  text-align: center;
+  padding: 72px 0 0;
+`;
+
+export const h5 = styled.h5`
+  ${global.fontFreightDispBold}
+  font-size: 40px;
+  margin: 0 0 15px;
+`;
+
+export const h6 = styled.h6`
+  ${global.fontRobotoLight}
+  font-size: 24px;
+  margin: 0 0 15px;
+  line-height: 35px;
+`;
+
+export const span = styled.span`
+  display: inline-block;
+  margin: 0;
+`;
+
+export const a = styled.a`
+  color: black !important;
+`;
+
+export const p = styled.p`
+  ${global.fontRobotoLight}
+  font-size: 20px;
+  margin: 0 0 15px;
+  line-height: 1.5;
+`;
+
+// export const banner = styled.img`
+//   width: 100%;
+//   height: auto;
+// `;
+
+export const clutchImage = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 320px;
+  margin-bottom: 20px;
+`;
+export const block = styled.div`
+  width: 100%;
+  right: 0;
+  position: absolute;
+  z-index: -1;
+`;
+
+
+export const aboutBg = styled.img`
+  width: 67%;
+  padding: 24% 0;
+  right: 0;
+  background: #f8f9ff;
+  position: absolute;
+  z-index: -1;
+`;
+
+export const blogContent = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const weDid = styled.div`
+  width: 65%;
+  position: relative;
+  left: 25%;
+  padding-left: 10%;
+`;
+
+export const numberRow = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 10vw 0;
+`;
+
+export const titleSection = styled.div`
+  width: 50%;
+  position: relative;
+  left: 25%;
+`;
+
+export const contentSection = styled.div.attrs(({ sectionIndex }) => ({
+  sectionIndex
+}))`
+  position: relative;
+  display: flex;
+  flex-direction: ${props =>
+    props.sectionIndex % 2 === 0 ? "row-reverse" : "inherit"};
+  background: #f8f9ff;
+  padding-bottom: 30px;
+`;
+
+export const screenshot = styled.img`
+  width: 100%;
+  margin-top: -6%;
+`;
+
+export const content = styled.div`
+  position: relative;
+  left: 14%;
+  padding-right: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 30%;
+`;
+
+export const imageBlock = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const caseRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 175px 0 0;
+`;
+
+export const caseSection = styled.div`
+  text-align: center;
+  margin-bottom: 35px;
+`;
+
+export const caseBlock = styled.div.attrs(({ imgPath }) => ({
+  imgPath
+}))`
+  position: relative;
+  background-image: url(${props => props.imgPath});
+  width: 100%;
+  background-repeat: no-repeat;
+  width: 364px;
+  height: 292px;
+  margin-bottom: 35px;
+  text-align: center;
+`;
+
+export const caseStudyImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export const caseBtn = styled.button`
+  position: relative;
+  font-size: 1em;
+  color: #fff;
+  top: 45%;
+  background: transparent;
+  border-radius: 50px;
+  border: 1px solid #fff;
+  box-shadow: none;
+  text-transform: uppercase;
+  padding: 11px 54px;
+  cursor: pointer;
+`;
