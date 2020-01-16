03ae88906e3a2da85ee1f37f185d27b358b0a9b1
kirkdc <kirk_dcosta@yahoo.com>
2020-01-16 20:28:15 +0530

    google ad page WIP

---
+++ src/containers/app-ad/commons/data.js
@@ -0,0 +1,104 @@
+import Flutter from '../../home/commons/Flutter';
+import NodeJs from '../../home/commons/NodeJs';
+import ReactJs from '../../home/commons/ReactJs';
+import ReactNative from '../../home/commons/ReactNative';
+import VueJs from '../../home/commons/VueJs';
+
+
+export const StrategyStats = [
+  {
+    number: "14",
+    title: "Team Players"
+  },
+  {
+    number: "USD 8+ million",
+    title: "Clients raised"
+  },
+  {
+    number: "150,000+",
+    title: "Users served"
+  },
+  {
+    number: "10+",
+    title: "Impact in countries"
+  },
+  {
+    number: "5+ years",
+    title: "Experience"
+  },
+  {
+    number: "50+",
+    title: "Projects"
+  }
+];
+
+export const specialize = [
+  {
+    img: "../static/images/home/website.svg",
+    title: "Web Development",
+    content:
+      "We build modern web applications of all shapes and sizes using the latest technologies."
+  },
+  {
+    img: "../static/images/home/backend.svg",
+    title: "Backend Architecture and Development",
+    content:
+      "We develop bulletproof APIs, sockets, and databases. 100% reliable, scalable, and high- performing."
+  },
+  {
+    img: "../static/images/home/mobileapps.svg",
+    title: "Mobile Apps",
+    content:
+      "Our unique mobile app development process allows us to rapidy build cross-platform apps at a fraction of the cost."
+  }
+];
+
+export const testimonialsList = [
+  {
+    img: "/static/images/home/bryce.jpeg",
+    heading: "Bryce York",
+    subHeading: "CTO and Co-founder, Radframe",
+    paragraph:
+      "Working with Naisheel and Codalyze has been a great experience. Communication has been great and heâ€™s always been willing to go the extra mile to get things done the way we want them - even when thereâ€™s some kind of misunderstanding. Weâ€™ve worked with Naisheel for about two years now and weâ€™ll continue to work together going forward."
+  },
+  {
+    img: "/static/images/home/lucas.jpeg",
+    heading: "Lucas K Allmon",
+    subHeading: "CTO, The Culture List",
+    paragraph:
+      "Naisheel knows his stuff, is both open to giving and receiving feedback, an collaborating on problems. Communicative and willing to both learn and use expertise. Awesome job!"
+  },
+  {
+    img: "/static/images/home/yianni.jpeg",
+    heading: "Yianni C",
+    subHeading: "CTO, The Culture List",
+    paragraph:
+      "Best experience I've ever had on upwork. Naisheel is a gem. He is great with communication, does very solid work, and can meet the deadlines. Would DEFINITELY recommend."
+  },
+  {
+    img: "/static/images/home/melvyn.jpeg",
+    heading: "Melvyn Kim",
+    subHeading: "Founder, Plan XYZ",
+    paragraph:
+      "Great developers. Only ending this to proceed with more tasks. Looking forward to working with you again!"
+  },
+  {
+    img: "",
+    heading: "Rahul",
+    subHeading: "Entrepreneur",
+    paragraph:
+      "I enjoyed working with him and will likely have additional jobs for him in the future. Will definitely hire him for next phase."
+  }
+];
+
+export const clientLogos = [
+  "/static/images/home/clients/betking.svg",
+  "/static/images/home/clients/fitcircle.svg",
+  "/static/images/home/clients/hotstar.svg",
+  "/static/images/home/clients/khelomore.svg",
+  "/static/images/home/clients/radframe.svg",
+  "/static/images/home/clients/DSP1.svg"
+];
+
+
+export const technologies = [Flutter, NodeJs, ReactJs, ReactNative, VueJs];
