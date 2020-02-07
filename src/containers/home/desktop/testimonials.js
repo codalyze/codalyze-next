9d69c1539e565872014c4dd1d148b01d8aff3e5a
Aura313 <tanyashah.395@gmail.com>
2020-02-07 18:24:05 +0530

    App ads WIP

--- src/containers/home/desktop/testimonials.js
+++ src/containers/home/desktop/testimonials.js
@@ -2,45 +2,64 @@ import React from "react";
 import * as Styled from "./styles";
 import TestimonialBg from "./images/testimonialBg";
 import TestimonialContentBg from "./images/testimonialContentBg";
-import {testimonialsList} from '../commons/data';
-import LazyLoad from 'react-lazyload';
+import { testimonialsList } from "../commons/data";
+import LazyLoad from "react-lazyload";
 
-import AwesomeSlider from 'react-awesome-slider';
+import AwesomeSlider from "react-awesome-slider";
 
-const Testimonial = () => (
-  <Styled.testimonialRow>
-    <Styled.testimonialSvg>
-       <TestimonialBg
-         width="100%"
-         height="100%"
-         viewBox="0 0 1920.998 1572.001"
-       />
-    </Styled.testimonialSvg>
-    <Styled.FlexContainer>
-    <Styled.testimonialTitle>What our clients are saying</Styled.testimonialTitle>
-      <Styled.testimonialSlide>
-        <AwesomeSlider>
-          {testimonialsList.map(t => (
-            <Styled.testimonialBlock key={t.heading}>
-              <Styled.testimonialContentSvg>
-                <TestimonialContentBg
-                  width="100%"
-                  height="100%"
-                  viewBox="0 0 1207.318 479.09"
-                />
-              </Styled.testimonialContentSvg>
-              <LazyLoad offset={100} once height={350}>
-                <Styled.testimonialImage src={t.img || "../static/images/home/testimonial-placeholder.png"} alt={t.heading}/>
-              </LazyLoad>
-              <Styled.testimonialHeading>{t.heading}</Styled.testimonialHeading>
-              <Styled.testimonialSubHeading>{t.subHeading}</Styled.testimonialSubHeading>
-              <Styled.testimonialParagraph>{t.paragraph}</Styled.testimonialParagraph>
-            </Styled.testimonialBlock>
-          ))}
-        </AwesomeSlider>
-      </Styled.testimonialSlide>
-    </Styled.FlexContainer>
-  </Styled.testimonialRow>
-);
+const Testimonial = props => {
+  console.log(props, "jfhhfhf");
+  return (
+    <Styled.testimonialRow className="abc">
+      {!props.hideSvg && (
+        <Styled.testimonialSvg>
+          <TestimonialBg
+            width="100%"
+            height="100%"
+            viewBox="0 0 1920.998 1572.001"
+          />
+        </Styled.testimonialSvg>
+      )}
+      <Styled.FlexContainer>
+        <Styled.testimonialTitle>
+          What our clients are saying
+        </Styled.testimonialTitle>
+        <Styled.testimonialSlide className="slider22">
+          <AwesomeSlider>
+            {testimonialsList.map(t => (
+              <Styled.testimonialBlock key={t.heading}>
+                <Styled.testimonialContentSvg>
+                  <TestimonialContentBg
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 1207.318 479.09"
+                  />
+                </Styled.testimonialContentSvg>
+                <LazyLoad offset={100} once height={350}>
+                  <Styled.testimonialImage
+                    src={
+                      t.img ||
+                      "../static/images/home/testimonial-placeholder.png"
+                    }
+                    alt={t.heading}
+                  />
+                </LazyLoad>
+                <Styled.testimonialHeading>
+                  {t.heading}
+                </Styled.testimonialHeading>
+                <Styled.testimonialSubHeading>
+                  {t.subHeading}
+                </Styled.testimonialSubHeading>
+                <Styled.testimonialParagraph>
+                  {t.paragraph}
+                </Styled.testimonialParagraph>
+              </Styled.testimonialBlock>
+            ))}
+          </AwesomeSlider>
+        </Styled.testimonialSlide>
+      </Styled.FlexContainer>
+    </Styled.testimonialRow>
+  );
+};
 
 export default Testimonial;
