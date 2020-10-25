9b216f0a903bda3eb15b1d1a34742222049016a9
Aura313 <tanyashah.395@gmail.com>
2020-10-25 11:55:09 +0530

    Update team details

--- src/containers/about-us/commons/team-social.js
+++ src/containers/about-us/commons/team-social.js
@@ -1,132 +1,218 @@
 export const teamMembers = [
-    {
-      id: 1,
-      image: "/static/images/about-us/Team/nv.jpeg",
-      name: "Naisheel Verdhan",
-      position: "CEO & Founder",
-      socialButtons: [{
+  {
+    id: 1,
+    image: "/static/images/about-us/Team/nv.jpeg",
+    name: "Naisheel Verdhan",
+    position: "Founder",
+    socialButtons: [
+      {
         type: "github",
-        url: "https://github.com/nverdhan"
-      },{
+        url: "https://github.com/nverdhan",
+      },
+      {
         type: "so",
-        url: "https://stackoverflow.com/users/3563619/naisheel-verdhan"
-      },{
+        url: "https://stackoverflow.com/users/3563619/naisheel-verdhan",
+      },
+      {
         type: "linkedin",
-        url: "https://www.linkedin.com/in/nverdhan/"
-      }]
-    },
-    {
-      id: 2,
-      image: "/static/images/about-us/Team/rishabhsaxena.jpg",
-      name: "Rishabh Saxena",
-      position: "Everything that's JavaScript",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/rishabhsaxena"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/rishabhsaxena"
-      }]
-    },
-    {
-      id: 3,
-      image: "https://avatars3.githubusercontent.com/u/7583399?s=460&v=4",
-      name: "Gaurav Kumar",
-      position: "Writes JS while he sleeps",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/g1aurav"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/g1aurav"
-      }]
-    },
-    {
-      id: 4,
-      image: "https://avatars0.githubusercontent.com/u/7762321?s=460&v=4",
-      name: "Aishwary Raj",
-      position: "React Native Ninja | Flutter expert",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/rajaishwary"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/rajaishwary/"
-      },{
-        type: "medium",
-        url: "https://medium.com/@rajaishwary"
-      }]
-    },
-    {
-      id: 5,
-      image: "https://avatars2.githubusercontent.com/u/9248554?s=460&v=4",
-      name: "Tanya Shah",
-      position: "UI/UX Rockstar",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/Aura313"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/tanya-shah-988952b5/"
-      }]
-    },
-    {
-      id: 6,
-      image: "https://avatars3.githubusercontent.com/u/24692748?s=460&v=4",
-      name: "Surabhi Sharma",
-      position: "NodeJS on the rocks",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/surabhi226005"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/surabhi-sharma-92b15691/"
-      }]
-    },
-    {
-      id: 7,
-      image: "https://avatars0.githubusercontent.com/u/29370738?s=460&v=4",
-      name: "Rakesh Lahre",
-      position: "NodeJS / VueJS",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/rakeshlahre"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/rakesh-lahre-1329bab5"
-      }]
-    },
-    {
-      id: 8,
-      image: "https://avatars1.githubusercontent.com/u/34913034?s=460&v=4",
-      name: "Hari Krushna Kartan",
-      position: "Cooks most authentic JS Biryani",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/hari-krushna"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/hari-krushna"
-      }]
-    },
-    {
-      id: 9,
-      image: "https://avatars3.githubusercontent.com/u/22146644?s=460&v=4",
-      name: "Krishna Singh",
-      position: "ReactJS / React Native",
-      socialButtons: [{
-        type: "github",
-        url: "https://github.com/randomkrishna"
-      },{
-        type: "linkedin",
-        url: "https://www.linkedin.com/in/randomkrishna"
-      }]
-    }
-  ];
+        url: "https://www.linkedin.com/in/nverdhan/",
+      },
+    ],
+  },
 
-  export const socialIcons = {
-    github: "/static/images/social-icons/github.png",
-    so: "/static/images/social-icons/so.svg",
-    medium: "/static/images/social-icons/medium.png",
-    linkedin: "/static/images/social-icons/linkedin.png"
-  };
+  {
+    id: 2,
+    image: "https://avatars3.githubusercontent.com/u/7583399?s=460&v=4",
+    name: "Gaurav Kumar",
+    position: "Writes JS while he sleeps",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/g1aurav",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/g1aurav",
+      },
+    ],
+  },
+  {
+    id: 3,
+    image: "https://avatars0.githubusercontent.com/u/7762321?s=460&v=4",
+    name: "Aishwary Raj",
+    position: "React Native Ninja | Flutter expert",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/rajaishwary",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/rajaishwary/",
+      },
+      {
+        type: "medium",
+        url: "https://medium.com/@rajaishwary",
+      },
+    ],
+  },
+  {
+    id: 4,
+    image: "https://avatars2.githubusercontent.com/u/9248554?s=460&v=4",
+    name: "Tanya Shah",
+    position: "UI/UX Rockstar",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/Aura313",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/tanya-shah-988952b5/",
+      },
+    ],
+  },
+  {
+    id: 5,
+    image: "https://avatars3.githubusercontent.com/u/24692748?s=460&v=4",
+    name: "Surabhi Sharma",
+    position: "NodeJS on the rocks",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/surabhi226005",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/surabhi-sharma-92b15691/",
+      },
+    ],
+  },
+  {
+    id: 6,
+    image: "https://avatars3.githubusercontent.com/u/22146644?s=460&v=4",
+    name: "Krishna Singh",
+    position: "ReactJS / React Native",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/randomkrishna",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/randomkrishna",
+      },
+    ],
+  },
+  {
+    id: 7,
+    image: "/static/images/about-us/Team/jp.jpeg",
+    name: "JyothiPrasad Uppalapu (JP)",
+    position: "ReactJS / NodeJS",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/UJPrasad",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/jyothiprasadu/",
+      },
+    ],
+  },
+  {
+    id: 8,
+    image: "/static/images/about-us/Team/ravi.jpg",
+    name: "Ravi Yadav",
+    position: "ReactJS / VueJS / NodeJS",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/logicallycorrect",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/ravi-yadav-b7165a113/",
+      },
+    ],
+  },
+  {
+    id: 9,
+    image: "/static/images/about-us/Team/kirk.jpeg",
+    name: "Kirk D'Costa",
+    position: "ReactJS / React Native / VueJS",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/kirkdc",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/kirkdcosta/",
+      },
+    ],
+  },
+  {
+    id: 10,
+    image: "/static/images/about-us/Team/deepak.jpeg",
+    name: "Deepak Yadav",
+    position: "ReactJS / React native",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/yadeepak",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/deepak-yadav-93092515b/",
+      },
+    ],
+  },
+  {
+    id: 11,
+    image: "/static/images/about-us/Team/siddharth.jpeg",
+    name: "Siddharth Trimal",
+    position: "ReactJS",
+    socialButtons: [
+      {
+        type: "github",
+        url: "https://github.com/siddharthtrimal",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/siddharth-trimal-6a9a6b114/",
+      },
+    ],
+  },
+  {
+    id: 12,
+    image: "/static/images/about-us/Team/kanchan.jpeg",
+    name: "Kanchan Jodha",
+    position: "Quality Assurance",
+    socialButtons: [
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/kanchan-jodha-0ba77a109/",
+      },
+    ],
+  },
+  {
+    id: 13,
+    image: "/static/images/about-us/Team/khyati.jpeg",
+    name: "Khyati Shah",
+    position: "HR, Recruitment / Ops and small talk",
+    socialButtons: [
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/khyatisshah/",
+      },
+    ],
+  },
+];
+
+export const socialIcons = {
+  github: "/static/images/social-icons/github.png",
+  so: "/static/images/social-icons/so.svg",
+  medium: "/static/images/social-icons/medium.png",
+  linkedin: "/static/images/social-icons/linkedin.png",
+};
